7ebf52926179ba06662921da3e8b0f4e5ba2c1b2
Wolf <wolf@zv.cx>
2024-08-31 10:35:41 -0400

    Typo: missing parens in call; unused variable

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -8,7 +8,7 @@ from dotx.cli import cli
 
 
 def test_install_normal_file():
-    with TemporaryDirectory() as source_package_root, TemporaryDirectory() as destination_root:
+    with TemporaryDirectory() as source_package_root:
         source_package_root_path = Path(source_package_root)
         file_path = Path("SIMPLE-FILE")
         (source_package_root_path / file_path).touch()
@@ -24,7 +24,7 @@ def test_install_normal_file():
 
 @pytest.mark.skip("Click isn't doing the right thing here.")
 def test_options_functions():
-    runner = CliRunner
+    runner = CliRunner()
     result = runner.invoke(cli, "--verbose --debug --dry-run debug")
 
     assert len(result.output) == 0
